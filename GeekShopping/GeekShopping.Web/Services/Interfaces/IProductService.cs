d278dfb4bfc9b9815085f5769735727180ec188a
monzondeveloper <monzondeveloper@gmail.com>
2024-12-20 18:32:36 -0300

    product service interface created

---
+++ GeekShopping/GeekShopping.Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllProductsAsync();
+        Task<ProductModel> GetProductByIdAsync(long id);
+        Task<ProductModel> CreateProductAsync(ProductModel product);
+        Task<ProductModel> UpdateProduct(ProductModel product);
+        Task<bool> DeleteProductByIdAsync(long id);
+    }
+}
